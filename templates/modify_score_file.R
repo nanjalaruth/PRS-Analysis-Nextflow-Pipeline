30137f3a767e83e5be923c2616003e40d9f41af4
nanjalaruth <ruthnanjala97@gmail.com>
2026-04-15 18:02:00 +0100

    modified score file

--- templates/modify_score_file.R
+++ templates/modify_score_file.R
@@ -11,35 +11,49 @@ pacman::p_load(
   data.table)
 
 # Load the files
-edit_file <- read.table("${edit_2}", header = TRUE, sep = "\\t")
-converted_file <- read.table("${edit_3}", header = TRUE, sep = "\\t")
+input_file <- read.table("${mod1}", header = TRUE, sep = "\\t")
+
+#edit_file <- read.table("{edit_2}", header = TRUE, sep = "\\t")
+#converted_file <- read.table("{edit_3}", header = TRUE, sep = "\\t")
 
 # Check if the files are similar
-are_files_similar <- identical(edit_file, converted_file)
+#are_files_similar <- identical(edit_file, converted_file)
 
-if (are_files_similar) {
+#if (are_files_similar) {
   # If the files are identical, just use one of them (e.g., edit_2)
-  print("Files are identical, using edit_2 directly.")
-  final_result <- edit_file
-} else {
+  #print("Files are identical, using edit_2 directly.")
+  #final_result <- edit_file
+#} else {
   # If the files are not identical, proceed with the merge
-  print("Files are not identical, proceeding with merge.")
+  #print("Files are not identical, proceeding with merge.")
   
   # Merge the files based on the SNP column
-  merged_file <- merge(edit_file, converted_file, by = "SNP", all.x = TRUE)
+  #merged_file <- merge(edit_file, converted_file, by = "SNP", all.x = TRUE)
   
   # Rename SNP column to Old_SNP
-  merged_file <- merged_file %>%
-    dplyr::rename(Old_SNP = SNP)
+  #merged_file <- merged_file %>%
+    #dplyr::rename(Old_SNP = SNP)
   
   # Create a new SNP column by combining lifted_chr_name and end_position without the "chr" prefix
-  merged_file\$SNP <- paste(merged_file\$chr_name, merged_file\$end_position, sep = ":")
+  #merged_file\$SNP <- paste(merged_file\$chr_name, merged_file\$end_position, sep = ":")
   
   # Use merged result as final output
-  final_result <- merged_file
+  #final_result <- merged_file
+#}
+
+# create a snp column
+if (!"SNP" %in% names(input_file)) {
+  chr_name_index <- which(names(input_file) == "hm_chr")
+  chr_pos_index  <- which(names(input_file) == "hm_pos")
+  
+  if (length(chr_name_index) == 0 || length(chr_pos_index) == 0) {
+    stop("Columns 'chr_name' or 'chr_position' not found.")
+  }
+  
+  input_file\$SNP <- paste0(input_file[[chr_name_index]], ":", input_file[[chr_pos_index]])
 }
 
-scoring_file <- data.frame(SNP = final_result\$SNP, A1 = final_result\$effect_allele, BETA = final_result\$effect_weight)
+scoring_file <- data.frame(SNP = input_file\$SNP, A1 = input_file\$effect_allele, BETA = input_file\$effect_weight)
 scoring_file_uniq <- scoring_file[!duplicated(scoring_file\$SNP), ]
 score_file <- na.omit(scoring_file_uniq)
 clean_score_file <- score_file[score_file[, 1] != "", , drop = FALSE]
